de7d4cb902d106f006329a639de64d9480213933
chester425 <chester425@mail.ru>
2019-07-09 01:00:26 +0300

    доработал проект по критериям

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
-var link = document.querySelector(".titel-form");
+var link = document.querySelector(".titel-form-wrapper");
 var popap = document.querySelector(".search");
+
 link.addEventListener("click", function (evt){
   evt.preventDefault();
   popap.classList.toggle("search-popap")
